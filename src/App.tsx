e3cd57b637fff12a20df8d7f2e78331937b81d7f
Illia <hetsko.ilya@gmail.com>
2025-07-12 02:54:26 +0200

    initial website version complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,122 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 import './App.scss';
+import { HomePage } from './modules/HomePage';
+import { ProductsPage } from './modules/PhonesPage';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { CartPage } from './modules/CartPage';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
+import { Footer } from './components/Footer/Footer';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
+import { useCart } from './modules/CartPage/components/CartContext/CartContext';
+import { Product } from './types/Product';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { cart, removeFromCart, increaseQuantity, decreaseQuantity } =
+    useCart();
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+  const toggleSidebar = () => {
+    setIsSidebarOpen(prev => !prev);
+  };
 
-export const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [productsIsLoading, setProductsIsLoading] = useState(false);
+  const [productsIsError, setProductsIsError] = useState(false);
+
+  const fetchData = useCallback(() => {
+    setProductsIsLoading(true);
+    setProductsIsError(false);
+
+    fetch('./api/products.json')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(() => {
+        setProductsIsError(true);
+      })
+      .finally(() => {
+        setProductsIsLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className={`app ${isSidebarOpen ? 'no-scroll' : ''}`}>
+      <Header onMenuClick={toggleSidebar} isOpen={isSidebarOpen} cart={cart} />
+      <div className="app__body">
+        <Sidebar isOpen={isSidebarOpen} />
+        <div className="app__content">
+          <main className="main">
+            <Routes>
+              <Route index element={<HomePage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route
+                path="/phones"
+                element={
+                  <ProductsPage
+                    title="Mobile phones"
+                    sortBy="phones"
+                    products={products}
+                    isLoading={productsIsLoading}
+                    isError={productsIsError}
+                    fetchData={fetchData}
+                  />
+                }
+              />
+              <Route
+                path="/tablets"
+                element={
+                  <ProductsPage
+                    title="Tablets"
+                    sortBy="tablets"
+                    products={products}
+                    isLoading={productsIsLoading}
+                    isError={productsIsError}
+                    fetchData={fetchData}
+                  />
+                }
+              />
+              <Route
+                path="/accessories"
+                element={
+                  <ProductsPage
+                    title="Accessories"
+                    sortBy="accessories"
+                    products={products}
+                    isLoading={productsIsLoading}
+                    isError={productsIsError}
+                    fetchData={fetchData}
+                  />
+                }
+              />
+              <Route path="/favorites" element={<FavoritesPage />} />
+              <Route
+                path="/cart"
+                element={
+                  <CartPage
+                    cart={cart}
+                    removeFromCart={removeFromCart}
+                    increaseQuantity={increaseQuantity}
+                    decreaseQuantity={decreaseQuantity}
+                  />
+                }
+              />
+              <Route
+                path="/:category/:productId"
+                element={
+                  <ProductDetailsPage
+                    products={products}
+                    fetchDataProducts={fetchData}
+                  />
+                }
+              />
+            </Routes>
+          </main>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
